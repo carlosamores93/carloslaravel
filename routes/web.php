6b0baf15d25d8b3eae43259abf17f2fd0e50a5f5
Carlos Salvador Amores Martinez <amorescarlos93@hotmail.com>
2018-03-04 20:24:20 +0100

    Vistas basicas0

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('frontend.layout');
 });
+
+Route::get('/home/', function () {
+    return view('frontend.home');
+});
+
+Route::get('/about/', function () {
+    return view('frontend.about');
+});
+
+Route::get('/contact/', function () {
+    return view('frontend.contact');
+});
