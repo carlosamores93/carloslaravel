9fe52e10dcc4222e462f4fd2d9de7daf2738751b
Carlos Salvador Amores Martinez <amorescarlos93@hotmail.com>
2018-03-08 00:49:14 +0100

    Cambios plantilla

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -68,30 +68,11 @@
                 <nav class="collapse navbar-collapse navbar-right" role="navigation">
                     <div class="main-menu">
                         <ul class="nav navbar-nav navbar-right">
-                            <li>
-                                <a href="/home/" >Home</a>
-                            </li>
+                            <li><a href="/home/" >Home</a></li>
                             <li><a href="/about/">About</a></li>
-                            <li><a href="service.html">Service</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <span class="caret"></span></a>
-                                <div class="dropdown-menu">
-                                    <ul>
-                                        <li><a href="404.html">404 Page</a></li>
-                                        <li><a href="gallery.html">Gallery</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <span class="caret"></span></a>
-                                <div class="dropdown-menu">
-                                    <ul>
-                                        <li><a href="blog-fullwidth.html">Blog Full</a></li>
-                                        <li><a href="blog-left-sidebar.html">Blog Left sidebar</a></li>
-                                        <li><a href="blog-right-sidebar.html">Blog Right sidebar</a></li>
-                                    </ul>
-                                </div>
-                            </li>
+                            <li><a href="/conocimientos/">Conocimientos</a></li>
+                            <li><a href="/educacion/">Educación</a></li>
+                            <li><a href="/experiencia/">Experiencia</a></li>
                             <li><a href="/contact/">Contact</a></li>
                         </ul>
                     </div>
@@ -154,7 +135,7 @@
                         </li>
                         <li>
                             <a href="http://wwww.twitter.com/themefisher" class="Twitter">
-                                <i class="ion-social-twitter"></i>
+                                <i class="ion-social-github"></i>
                             </a>
                         </li>
                         <li>
