8079aad1f06d442cb402c69687618843f6ecf2fa
carlosamores <amorescarlos93@hotmail.com>
2018-08-03 23:29:09 +0200

    mas

--- app/Http/Controllers/Frontend/AboutController.php
+++ app/Http/Controllers/Frontend/AboutController.php
@@ -26,9 +26,9 @@ class AboutController extends Controller{
     public function bragas(){
         
         $a_meta_seo = array();
-        $a_meta_seo['title'] = 'Ventas de bragas usadas. Lencería y bikinis usadas en Madrid';
-        $a_meta_seo['description'] = '';
-        $a_meta_seo['canonical'] = env('APP_URL').'Venta de bragas usadas. Eres de esos hombres fetichistas que les gusta la ropa interior feminina usadas? Ahora es el momento, tienes gran variedad de ofertas por Internet, he creado este articulo porque una amiga está interesada en vender este tipo de ropa interior. Solo tienes con contactar con ella o con el admin de esta web. Un saludo :)';
+        $a_meta_seo['title'] = 'Venta de bragas o tangas usadas. Lencería y bikinis usadas en Madrid';
+        $a_meta_seo['description'] = 'Venta de bragas usadas. Eres de esos hombres fetichistas que les gusta la ropa interior feminina usadas? Ahora es el momento, tienes gran variedad de ofertas por Internet, he creado este articulo porque una amiga está interesada en vender este tipo de ropa interior. Solo tienes con contactar con ella o con el admin de esta web. Un saludo :)';
+        $a_meta_seo['canonical'] = env('APP_URL').'/compra-venta-bragas-usadas.html';
 
         return view('frontend.postBragas.bragas', compact('a_meta_seo'));
     }
