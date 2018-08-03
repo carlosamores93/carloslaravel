8be93ddce200570fa66d99d283312fac28e9782d
carlosamores <amorescarlos93@hotmail.com>
2018-08-03 22:43:31 +0200

    Foto brgs

--- resources/views/frontend/postBragas/bragas.blade.php
+++ resources/views/frontend/postBragas/bragas.blade.php
@@ -29,10 +29,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="post-img">
+                <div class="post-img text-center">
 
                     <h2> Como funciona la compra ventas de bragas usadas</h2>
-                    <img class="img-responsive" alt="" src="images/blog/post-1.jpg">
+                    <img class="img-responsive" alt="Bragas usadas" src="images/blog/bragas.jpg" style="display:unset;">
                 </div>
                 <div class="post-content">
                     <p>
@@ -51,7 +51,9 @@
                         <li>Lorem ipsum dolor amet.</li>
                     </ol>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus nostrum consectetur voluptatibus, odio ea ab distinctio, nulla asperiores facere sequi dolorum molestiae magni sed velit officia rerum illo necessitatibus consequatur maiores magnam possimus voluptas suscipit praesentium iste! Praesentium, modi, illum. Sint quis eos expedita porro voluptatum reiciendis minus vitae atque deleniti eligendi nulla, dolorem adipisci, assumenda sunt modi suscipit inventore hic nostrum veniam, ea accusantium quisquam! Ipsum odio, ducimus magnam nam ad quaerat soluta, ab laudantium beatae eius iusto fugit blanditiis laboriosam cupiditate dolor aut nulla a quam dolores? Unde, sunt, explicabo sapiente quos reiciendis iste fuga atque esse voluptatem.
+                        <strong>
+                            Si estas intersado/a en la compra de bragas usadas no dudes en contactarnos. Si prefieres hacerlo por telefono puedes llamar al  630 25 41 59 y preguntar por Lorena Miranda.
+                        </strong>
                     </p>
                 </div>
                 
