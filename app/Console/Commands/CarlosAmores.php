4bd26bf0d6d134b16210b0d4051bdcef8894f532
carlosamores <amorescarlos93@hotmail.com>
2018-08-08 00:44:10 +0200

    mas

--- app/Console/Commands/CarlosAmores.php
+++ app/Console/Commands/CarlosAmores.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
 
 class CarlosAmores extends Command
 {
@@ -38,11 +39,114 @@ class CarlosAmores extends Command
      */
     public function handle()
     {
+
+        $test = array('Y' => 1896, 'M' => 8, 'D' => 7);
+        $msg = $this->recordarCumpleaños('Test', $test);
+        $this->info($msg);
+
+/*
+        $carlosCumple = array('Y' => 1993, 'M' => 3, 'D' => 1);
+        $msg = $this->recordarCumpleaños('Carlos Amores', $carlosCumple);
+        $this->info($msg);
+
+        $camila = array('Y' => 2016, 'M' => 12, 'D' => 17);
+        $msg = $this->recordarCumpleaños('Camila Amores', $camila);
+        $this->info($msg);
+*/
+
+        if($msg === 'NO'){
+            $asunto = "Test NO";
+            $msg = "Mesaje al correo, TEST NO";
+            mail('admin@amorescarlos.com', $asunto, $msg);
+            mail('amorescarlos93@hotmail.com', $asunto, $msg);
+            mail('carlos-ecua-23@hotmail.com', $asunto, $msg);
+        }else{
+            $asunto = "Test SI";
+            $msg = "Mesaje al correo, TEST SI";
+            mail('admin@amorescarlos.com', $asunto, $msg);
+            mail('amorescarlos93@hotmail.com', $asunto, $msg);
+            mail('carlos-ecua-23@hotmail.com', $asunto, $msg);
+        }
+
+        /*
         $asunto = "Cumple Carlos Amores";
         $msg = "Mesaje al correo";
         mail('admin@amorescarlos.com', $asunto, $msg);
         mail('amorescarlos93@hotmail.com', $asunto, $msg);
         mail('carlos-ecua-23@hotmail.com', $asunto, $msg);
-        $this->info("Yaaaaaa");
+        */
+        //$this->probandoFechas();
+    }
+
+    public function recordarCumpleaños($name, $fecha){
+        $msg = 'NO';
+        $dateNow = Carbon::now();
+        if($dateNow->month === $fecha['M'] && $dateNow->day === $fecha['D']){
+            $edad = $dateNow->year - $fecha['Y'];
+            $msg = $name . " cumple hoy " . $edad . ' años';
+        }
+        return $msg;
+    }
+
+
+    public function probandoFechas(){
+        $now = Carbon::now();
+        $this->info('Now: ' . $now);
+
+
+        $today = Carbon::today();
+        $this->info('Today: ' . $today);
+
+
+        $tomorrow = Carbon::tomorrow('Europe/London');
+        $this->info('Tomorrow: ' . $tomorrow);
+
+
+        $yesterday = Carbon::yesterday();
+        $this->info('Yesterday: ' . $yesterday);  
+
+
+        $carbon = new \Carbon\Carbon();
+        $date = $carbon->now();
+        $this->info('Date now: ' . $date);
+        $this->info('Date now->toDateTimeString(): ' . $date->toDateTimeString());
+        $this->info('Date now YEAR: ' . $date->year);
+
+
+        /*
+        Carbon::createFromDate($year, $month, $day, $tz);
+        Carbon::createFromTime($hour, $minute, $second, $tz);
+        Carbon::createFromTimeString("$hour:$minute:$second", $tz);
+        Carbon::create($year, $month, $day, $hour, $minute, $second, $tz);
+         */
+
+        $xmasThisYear = Carbon::createFromDate(null, 12, 25);
+        // Year defaults to current year
+        $this->info('xmasThisYear: ' . $xmasThisYear);
+
+
+        $Y2K = Carbon::create(2000, 1, 1, 0, 0, 0); 
+        // equivalent to Carbon::createMidnightDate(2000, 1, 1)
+        $this->info('Y2K: ' . $Y2K);
+
+
+        $alsoY2K = Carbon::create(1999, 12, 31, 24);
+        $this->info('alsoY2K: ' . $alsoY2K);
+
+
+        $noonLondonTz = Carbon::createFromTime(12, 0, 0, 'Europe/London');
+        $this->info('noonLondonTz: ' . $noonLondonTz);
+
+
+        $teaTime = Carbon::createFromTimeString('17:00:00', 'Europe/London');
+        $this->info('teaTime: ' . $teaTime);
+
+
+        try {
+            Carbon::createSafe(2000, 1, 35, 13, 0, 0);
+        } catch (\Carbon\Exceptions\InvalidDateException $exp) {
+            $this->error($exp->getMessage());
+        }
+
     }
 }
