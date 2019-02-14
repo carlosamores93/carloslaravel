50324ee9f348b9295c8e2e05fcf5e4f9f5348d10
Carlos <amorescarlos93@hotmail.com>
2019-02-15 00:59:33 +0100

    Combimnaiuones

---
+++ app/Console/Commands/Combine.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Combine extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'combine';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tables = ['1', '2', '3', '4', '4', '6'];
+        for ($i=2; $i < 7; $i++) { 
+            # code...
+            $array_combinaciones = $this->combinaciones($tables, $i);
+            dd($array_combinaciones);
+        }
+    }
+
+    public function potencia($c){
+        if(count($c) == 0){
+            return [[]];
+        }
+        $tmp = array_pop($c);
+        $r = $this->potencia($c);
+        $otro_array = array(array($tmp));
+        foreach ($r as $rr) {
+            if(count($rr) != 0){
+                array_push($rr, $tmp);
+                array_push($otro_array, $rr);
+            }
+        }
+        $y = array_merge($r, $otro_array);
+        return $y;
+    }
+
+    public function combinaciones($c, $n){
+        $resp = [];
+        foreach ($this->potencia(($c)) as $s) {
+            if(count($s) == $n){
+                array_push($resp, $s);
+            }
+        }
+        return $resp;
+    }
+}
